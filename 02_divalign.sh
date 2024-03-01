f58f173858189afd2860b7c285f8d4c14e8df8c5
AAnnan <ahrmad.annan@unil.ch>
2024-03-01 10:11:03 +0100

    move peak/cell matrix creation to new script

--- 02_divalign.sh
+++ 02_divalign.sh
@@ -149,54 +149,15 @@ END {
 }' TEMP.sam > TEMPHEADER.sam
 
 # Append the new header to the RG-replaced SAM file and convert to BAM
-{ cat TEMPHEADER.sam; grep -v '^@' TEMPRG.sam; } | ${PathSamtools} sort --threads ${threads} -o ${RGID}.bam -
+{ cat TEMPHEADER.sam; grep -v '^@' TEMPRG.sam; } | ${PathSamtools} sort --threads ${threads} -o TEMP.bam -
 
 # Mark duplicates
-java -jar ${PathPicard} MarkDuplicates I=${RGID}.bam O=${PathOutputBam} M=${PathOutputPicardDupStats} REMOVE_DUPLICATES=${RemDups}
+java -jar ${PathPicard} MarkDuplicates I=TEMP.bam O=${PathOutputBam} M=${PathOutputPicardDupStats} REMOVE_DUPLICATES=${RemDups}
 
 #Remove temp files
-rm TEMP.sam TEMPRG.sam TEMPHEADER.sam ${RGID}.bam
-
-### To create the peak/cell matrix
-# Convert Peak BED file to SAF format
-#awk 'OFS="\t" {print $1"."$2"."$3"."$4, $1, $2+1, $3, "."}' ${inputBEDFile} > ${outputSAFFile}
-#sed -i '1s/^/GeneID\tChr\tStart\tEnd\tStrand\n/' ${outputSAFFile}
-
-# if you're not confident in your sed version (ie using a mac), change the above line to this:
-#echo -e "GeneID\tChr\tStart\tEnd\tStrand" | cat - ${inputBEDFile} > temp && mv temp ${outputSAFFile}
-
-#Run featureCounts to count number of reads per peak
-# install subread/featurecounts: "conda install bioconda::subread"
-#featureCounts -T ${threads} -a ${outputSAFFile} -F SAF -p --byReadGroup -O -o ${RGID}.tsv ${RGID}.bam
+rm TEMP.sam TEMPRG.sam TEMPHEADER.sam TEMP.bam
 
 
 # UNUSED CODE
 # Get alignement stats
-#samtools flagstat -@ ${threads} ${RGID}.bam
-
-# Split by BC (WILL CRASH YOUR SYSTEM creates too many files)
-#samtools split -@ ${threads} --max-split '-1' -u NoBarcode.sam -f "%*_%\!" -d 'CB' -v ${RGID}.bam
-
-#Get list of unique BCs in SAM file
-#samtools view temp.sam | cut -f 12- | tr "\t" "\n"  | grep  "^CB:Z:"  | cut -d ':' -f 3 | sort | uniq > uniq_BCs
-
-# TESTING CODE
-#macs3 callpeak --treatment TESTDONE.bam \
-#--format BAMPE \
-#--gsize hs \
-#--nomodel \
-#--nolambda \
-#--keep-dup auto \
-#--qvalue 0.01 \
-#--call-summits \
-#--outdir callpeak_TEST \
-#--name merged \
-#--verbose 2
-
-#awk 'OFS="\t" {print $1"."$2"."$3"."$4, $1, $2+1, $3, "."}' TEST.bed > TEST.saf
-#sed -i '1s/^/GeneID\tChr\tStart\tEnd\tStrand\n/' TEST.saf
-#featureCounts -T 16 -a TEST.saf -F SAF -p --byReadGroup -O -o TESTDONE.tsv TESTDONE.bam
-
-#PathPicard="/home/ahrmad/picard.jar"
-#RemDups=false
-#java -jar ${PathPicard} MarkDuplicates I=TESTDONE.bam O=TESTDONE_MarkedDup.bam M=TESTDONE_DupMetrics.txt REMOVE_DUPLICATES=${RemDups}
+#samtools flagstat -@ ${threads} ${PathOutputBam}
